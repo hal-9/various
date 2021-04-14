f6c888c98c760e7e473b296f79cc23cb09024ffd
Tuncay Aydin <tunikb@posteo.de>
2021-04-14 20:03:24 +0200

    added darkmode button

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Berlin&units=metric&app
 .then(response => response.json())
 .then(data => console.log(data.main.temp));
 
+const darkModeBtn = document.querySelector('.darkModeButton');
+darkModeBtn.addEventListener('click', () => {
+  const darkModeBtn = document.querySelector('.darkModeButton');
+  if (darkModeBtn.getAttribute('fill') === "#000000") {
+    darkModeBtn.setAttribute('fill', '#26D07C')
+  } else {
+    darkModeBtn.setAttribute('fill', '#000000')
+  }
+
+  const logo = document.querySelector('.logo');
+  if (logo.getAttribute('fill') === "#000000") {
+    logo.setAttribute('fill', '#26D07C')
+  } else {
+    logo.setAttribute('fill', '#000000')
+  }
+  document.body.classList.toggle('darkMode');
+})
 
 const generateTime = () => {
   const rawTime = new Date();

ae0ae75a5200846e1c05e256c091fc41f4e6aa23
Tuncay Aydin <tunikb@posteo.de>
2021-04-18 16:09:07 +0200

    cleanup generatetime

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Berlin&units=metric&appid=998827d16513d893f0e9b7e95eaaea4a')
-.then(response => response.json())
-.then(data => console.log(data.main.temp));
+  .then(response => response.json())
+  .then(data => data.main.temp)
 
 const darkModeBtn = document.querySelector('.darkModeButton');
 darkModeBtn.addEventListener('click', () => {
@@ -25,14 +25,12 @@ darkModeBtn.addEventListener('click', () => {
 
 const generateTime = () => {
   const rawTime = new Date();
-  const hours = rawTime.getHours();
-  const minutes = rawTime.getMinutes();
-  if (minutes < 10)  {
-    const minutesWithZero = `0${minutes}`
-    document.querySelector('.time').innerHTML = `${hours}:${minutesWithZero} `;
-  } else {
-    document.querySelector('.time').innerHTML = `${hours}:${minutes} `;
-  }
-}
+  const rawHours = rawTime.getHours();
+  const rawMinutes = rawTime.getMinutes();
 
+  const finalHours = rawHours.toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping: false});
+  const finalMinutes = rawMinutes.toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping: false});
+
+  document.querySelector('.time').innerHTML = `${finalHours}:${finalMinutes} `;
+}
 setInterval(generateTime, 100);

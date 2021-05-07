4857de300ff17cf18a7be8572b72125079608076
Tuncay Aydin <tunikb@posteo.de>
2021-05-07 16:07:02 +0200

    spotify display built

--- script.js
+++ script.js
@@ -69,3 +69,102 @@ setInterval(function() {
   const randomPick = Math.floor((Math.random() * oAnimations.length));
   oAnimations[randomPick].style.display="block";
 }, 5000);
+
+const redirect_uri = "http://127.0.0.1:5500/index.html"
+const AUTHORIZE = "https://accounts.spotify.com/authorize"
+const client_id = "e1418b9264424319a857c3e2b53b0eb6"
+const client_secret = "7ebfc44506374ab7a5e6809e54930d4e"
+
+function requestAuthorization(){
+  // console.log('requestAuthorization fired');
+  let url = AUTHORIZE;
+  url += "?client_id=" + client_id;
+  url += "&response_type=code";
+  url += "&redirect_uri=" + encodeURI(redirect_uri);
+  url += "&show_dialog=true";
+  url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+  window.location.href = url;
+}
+
+const code = ""
+
+function fetchAccessToken(code){
+  // console.log('fetchAccessToken fired');
+  let body = "grant_type=authorization_code";
+  body += "&code=" + code;
+  body += "&redirect_uri=" + encodeURI(redirect_uri);
+  // console.log(body);
+  callAuthorizationApi(body);
+}
+
+function refreshAccessToken(){
+  // console.log('refresh accesstoken fired');
+  refresh_token = localStorage.getItem("refresh_token");
+  let body = "grant_type=refresh_token";
+  body += "&refresh_token=" + refresh_token;
+  body += "&client_id=" + client_id;
+  callAuthorizationApi(body);
+}
+
+function callAuthorizationApi(body) {
+  // console.log('callAuthorizationApi fired');
+  fetch('https://accounts.spotify.com/api/token', {
+    method: "POST",
+    body: body,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Authorization": "Basic " + btoa(client_id + ":" + client_secret),
+    }
+  })
+  .then(response => handleAuthorizationResponse(response));
+}
+
+function handleAuthorizationResponse(response) {
+  // console.log('handleAuthorizationFired');
+  // console.log(response);
+  if (response.status === 200) {
+    // console.log("if fired");
+    response.json()
+    .then(bodyData => {
+      if (bodyData.access_token !== undefined) {
+        // console.log('access_token ist defined')
+        access_token = bodyData.access_token;
+        localStorage.setItem("access_token", access_token);
+      }
+      if (bodyData.refresh_token !== undefined) {
+        refresh_token = bodyData.refresh_token;
+        localStorage.setItem("refresh_token", refresh_token);
+      }
+    })
+  }
+  else if (response.status === 400) {
+    // console.log('else if fired')
+    refreshAccessToken();
+    }
+  else {
+    alert('there was an error with code ' + response.status);
+  }
+}
+fetchAccessToken(code);  
+
+let albumName = "";
+let songName = "";
+let artistName = "";
+
+
+fetch('https://api.spotify.com/v1/me/player/currently-playing?market=DE', {
+  headers: {
+    Accept: "application/json",
+    Authorization: "Bearer " + localStorage.getItem("access_token"),
+    "Content-Type": "application/json"
+  }
+})
+.then(response => response.json())
+.then(data => {
+  let albumName = data.item.album.name;
+  let songName = data.item.name;
+  let artistName = data.item.artists[0].name;
+  document.querySelector('.footer__spotify').innerHTML = `${albumName}, ${artistName} - ${songName}`; 
+  // console.log('The currently playing track is called ' + songName + ' by ' + artistName + ' off the Album ' + albumName + ' .'); 
+});
+
